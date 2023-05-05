46b15ff2f0af5ed578fc3d313c708e520befd1a1
XiyvEbsty <2503861416@qq.com>
2023-05-05 16:45:07 +0800

    updata

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,45 +1,29 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    char *filename = "text.txt";
-    char *parameter = "-c";
-    int count = 0, word_count = 0, prev_space = 1;
+int main()
+{
     FILE *fp;
+    char ch;
+    int count = 0;
 
-    if (argc > 1) {
-        parameter = argv[1];
-        if (argc > 2) {
-            filename = argv[2];
-        }
-    }
+    fp = fopen("hamlet.txt", "r");
 
-    fp = fopen(filename, "r");
     if (fp == NULL) {
-        printf("Error opening file.\n");
-        return 1;
+        printf("File does not exist.\n");
+        return 0;
     }
 
-    int c;
-    while ((c = fgetc(fp)) != EOF) {
+    while ((ch = fgetc(fp)) != EOF) {
         count++;
-        if (c == ' ' || c == '\n' || c == '\t') {
-            prev_space = 1;
-        } else if (prev_space) {
-            word_count++;
-            prev_space = 0;
-        }
     }
 
     fclose(fp);
 
-    if (parameter[1] == 'c') {
-        printf("Character count: %d\n", count);
-    } else if (parameter[1] == 'w') {
-        printf("Word count: %d\n", word_count);
-    } else {
-        printf("Invalid parameter.\n");
-        return 1;
-    }
+    printf("Number of characters: %d\n", count);
 
     return 0;
+
+
+
 }
+
